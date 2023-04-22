0a220224564e75f0257fa40fe26f90a9af4cf2ab
Hưng Trần <100851238+HungTran24@users.noreply.github.com>
2023-04-22 17:33:25 +0700

    Update ProcedureTieuBieu.sql

--- ProcedureTieuBieu.sql
+++ ProcedureTieuBieu.sql
@@ -1,33 +1,72 @@
----Món ?n
--- Thêm 
-CREATE OR REPLACE PROCEDURE THEM_MON_AN(
-    p_id IN MON_AN.ID%TYPE,
-   p_ten_mon IN MON_AN.TEN_MON%TYPE,
-   p_mo_ta IN MON_AN.MO_TA%TYPE,
-   p_loai IN MON_AN.LOAI%TYPE,
-   p_hinh_anh IN BLOB
-)
-IS
+---MÃ³n ?n
+-- ThÃªm 
+create or replace PROCEDURE THEM_MON_AN (
+    P_ID IN NUMBER,
+    P_TEN_MON IN NVARCHAR2,
+    P_MO_TA IN NVARCHAR2,
+    P_LOAI IN NVARCHAR2,
+    P_TEN_ANH IN VARCHAR2
+) IS
+    L_BLOB BLOB;
+    L_BFILE       BFILE;
+    L_DEST_OFFSET INTEGER:=1;
+    L_SRC_OFFSET  INTEGER:=1;
+    L_LOBMAXSIZE  CONSTANT INTEGER := DBMS_LOB.LOBMAXSIZE;
 BEGIN
-   INSERT INTO MON_AN(ID, TEN_MON, MO_TA, LOAI, HINH_ANH) 
-   VALUES (p_id, p_ten_mon, p_mo_ta, p_loai, p_hinh_anh);
+    INSERT INTO MON_AN (
+        ID,
+        TEN_MON,
+        MO_TA,
+        LOAI,
+        HINH_ANH
+    ) VALUES (
+        P_ID,
+        P_TEN_MON,
+        P_MO_TA,
+        P_LOAI,
+        EMPTY_BLOB()
+    ) RETURN HINH_ANH INTO L_BLOB;
+    L_BFILE := BFILENAME('ANH_MON_AN', P_TEN_ANH);
+    DBMS_LOB.FILEOPEN(L_BFILE, DBMS_LOB.FILE_READONLY);
+    DBMS_LOB.LOADBLOBFROMFILE (L_BLOB, L_BFILE, L_LOBMAXSIZE, L_DEST_OFFSET, L_SRC_OFFSET);
+    DBMS_LOB.FILECLOSE(L_BFILE);
+    DBMS_OUTPUT.PUT_LINE('SIZE OF THE IMAGE IS: '
+        || DBMS_LOB.GETLENGTH(L_BLOB));
+    COMMIT;
 END;
 
 -- C?p nh?t 
-CREATE OR REPLACE PROCEDURE CAP_NHAT_MON_AN(
-   p_id IN MON_AN.ID%TYPE,
-   p_ten_mon IN MON_AN.TEN_MON%TYPE,
-   p_mo_ta IN MON_AN.MO_TA%TYPE,
-   p_loai IN MON_AN.LOAI%TYPE,
-   p_hinh_anh IN BLOB
-)
-IS
+create or replace PROCEDURE UPDATE_MON_AN (
+    P_ID IN NUMBER,
+    P_TEN_MON IN NVARCHAR2,
+    P_MO_TA IN NVARCHAR2,
+    P_LOAI IN NVARCHAR2,
+    P_TEN_ANH IN VARCHAR2
+) IS
+    L_BLOB BLOB;
+    L_BFILE BFILE;
+    L_DEST_OFFSET INTEGER:=1;
+    L_SRC_OFFSET INTEGER:=1;
+    L_LOBMAXSIZE CONSTANT INTEGER := DBMS_LOB.LOBMAXSIZE;
 BEGIN
-   UPDATE MON_AN SET TEN_MON = p_ten_mon, MO_TA = p_mo_ta, LOAI = p_loai, HINH_ANH = p_hinh_anh 
-   WHERE ID = p_id;
+    UPDATE MON_AN SET
+        TEN_MON = P_TEN_MON,
+        MO_TA = P_MO_TA,
+        LOAI = P_LOAI,
+        HINH_ANH = EMPTY_BLOB()
+    WHERE ID = P_ID
+    RETURNING HINH_ANH INTO L_BLOB;
+
+    L_BFILE := BFILENAME('ANH_MON_AN', P_TEN_ANH);
+    DBMS_LOB.FILEOPEN(L_BFILE, DBMS_LOB.FILE_READONLY);
+    DBMS_LOB.LOADBLOBFROMFILE(L_BLOB, L_BFILE, L_LOBMAXSIZE, L_DEST_OFFSET, L_SRC_OFFSET);
+    DBMS_LOB.FILECLOSE(L_BFILE);
+
+    DBMS_OUTPUT.PUT_LINE('SIZE OF THE IMAGE IS: ' || DBMS_LOB.GETLENGTH(L_BLOB));
+    COMMIT;
 END;
 
--- Xóa
+-- XÃ³a
 CREATE OR REPLACE PROCEDURE XOA_MON_AN(
    p_id IN MON_AN.ID%TYPE
 )
@@ -36,10 +75,27 @@ BEGIN
    DELETE FROM MON_AN
    WHERE ID = p_id;
 END;
-
+---Xuat cac mon mon an co the nau dua vao nguyen lieu trong kho (id,soluong)
+---xuat ra man hinh id va so luong mon an co the nau va con tro chua id,soluong moanan
+create or replace PROCEDURE MON_AN_FROM_NGUYEN_LIEU(P_CUR OUT SYS_REFCURSOR) AS
+    V_ID number;
+    V_SO_LUONG NGUYEN_LIEU_MON_AN.SO_LUONG%TYPE;
+BEGIN
+    OPEN P_CUR FOR
+        SELECT NLM.ID_MON, MIN(NL.SO_LUONG_TRONG_KHO / NLM.SO_LUONG) 
+        FROM NGUYEN_LIEU_MON_AN NLM, NGUYEN_LIEU NL 
+        where NL.ID = NLM.ID_NL
+        GROUP BY NLM.ID_MON;
+    loop
+     fetch p_cur into v_id,v_so_luong;
+       exit when p_cur%NOTFOUND;
+       DBMS_OUTPUT.PUT_LINE('ID_MON: ' || V_ID || ', SO_LUONG_MON_AN: ' || V_SO_LUONG);
+    end loop;
+    close P_CUR;
+end;
 
 --- b?ng NHAN_VIEN
--- Thêm
+-- ThÃªm
 CREATE OR REPLACE PROCEDURE THEM_NHAN_VIEN(
    p_id NHAN_VIEN.ID%TYPE,
    p_ten IN NHAN_VIEN.TEN%TYPE,
@@ -70,7 +126,7 @@ BEGIN
    WHERE ID = p_id;
 END;
 
--- Xóa
+-- XÃ³a
 CREATE OR REPLACE PROCEDURE XOA_NHAN_VIEN(
    p_id IN NHAN_VIEN.ID%TYPE
 )
