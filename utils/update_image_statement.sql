b42618b563b2335cc99cefc7c12169c17f37ba8e
sang <sangfc774@gmail.com>
2023-05-22 19:38:12 +0700

    adding

--- utils/update_image_statement.sql
+++ utils/update_image_statement.sql
@@ -1,23 +1,28 @@
-COMMIT;
 
+
+CREATE or replace DIRECTORY DIRECT_OF_IMAGE AS 'D:\LIST_MENU\food'; --Tạo đường dẫn tới file chứa file ảnh ở đây là file pictosave chứa ảnh quysung.jpg
+
+
+SELECT * FROM DBA_DIRECTORIES; --kiểm tra xem đã tạo đường dẫn thành công chưa
+
+--Sau đây là đoạn code để insert ảnh 
+COMMIT;
+set serveroutput on;
 DECLARE
-    L_BLOB        BLOB;
-    L_BFILE       BFILE;
-    L_DEST_OFFSET INTEGER:=1;
-    L_SRC_OFFSET  INTEGER:=1;
-    L_LOBMAXSIZE  CONSTANT INTEGER := DBMS_LOB.LOBMAXSIZE;
+l_blob BLOB;
+l_bfile BFILE;
+l_dest_offset INTEGER:=1;
+l_src_offset INTEGER:=1;
+l_lobmaxsize CONSTANT INTEGER := DBMS_LOB.LOBMAXSIZE;
 BEGIN
-    UPDATE MON_AN
-    SET
-        HINH_ANH = EMPTY_BLOB(
-        )
-    WHERE
-        TEN_MON = 'Hamburger' RETURNING HINH_ANH INTO L_BLOB;
-    L_BFILE := BFILENAME('DIRECT_OF_IMAGE', 'Hamburger.png');
-    DBMS_LOB.FILEOPEN(L_BFILE, DBMS_LOB.FILE_READONLY);
-    DBMS_LOB.LOADBLOBFROMFILE (L_BLOB, L_BFILE, L_LOBMAXSIZE, L_DEST_OFFSET, L_SRC_OFFSET);
-    DBMS_LOB.FILECLOSE(L_BFILE);
-    DBMS_OUTPUT.PUT_LINE('SIZE OF THE IMAGE IS: '
-        || DBMS_LOB.GETLENGTH(L_BLOB));
-    COMMIT;
+update MON_AN
+set MON_AN.HINH_ANH = empty_blob()
+where MON_AN.TEN_MON = 'French fries'
+RETURNING MON_AN.HINH_ANH INTO l_blob;
+l_bfile := BFILENAME('DIRECT_OF_IMAGE', 'French_fries.png');
+DBMS_LOB.FILEOPEN(l_bfile, DBMS_LOB.file_readonly);
+DBMS_LOB.loadblobfromfile (l_blob,l_bfile,l_lobmaxsize, l_dest_offset, l_src_offset);
+DBMS_LOB.fileclose(l_bfile);
+DBMS_OUTPUT.PUT_LINE('SIZE OF THE IMAGE IS: '|| DBMS_LOB.GETLENGTH(l_blob));
+COMMIT;
 END;
